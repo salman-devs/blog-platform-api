b0c515e66789f1f3ad6faf2fe2a7c77750e66397
salman-devs <salmanxtp@gmail.com>
2026-03-25 11:14:12 +0530

    add user model and create users table

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from app.database import engine, Base
+from app.models import User
+
+app=FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+@app.get("/")
+
+def root():
+    return {"message": "Blog API running"}
