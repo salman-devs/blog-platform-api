d73eb633d4c264c8784c9524f310682193950ad0
salman-devs <salmanxtp@gmail.com>
2026-03-29 12:45:45 +0530

    Add Alembic migrations and initialize database schema

---
+++ alembic/env.py
@@ -0,0 +1,71 @@
+from logging.config import fileConfig
+import os
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+from alembic import context
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+config = context.config
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set in environment variables")
+
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
+
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+
+from app.database import Base
+from app.models.user import User
+from app.models.post import Post
+from app.models.comment import Comment
+from app.models.like import likes
+
+target_metadata = Base.metadata
+
+
+def run_migrations_offline():
+    """Run migrations in 'offline' mode."""
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        compare_type=True
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    """Run migrations in 'online' mode."""
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            compare_type=True
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
--- app/main.py
+++ app/main.py
@@ -8,7 +8,6 @@ app=FastAPI(
     version="1.0.0"
 )
 
-Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 
