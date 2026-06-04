52503886322e496b27d927aa3e07be0e972948a7
salman-devs <salmanxtp@gmail.com>
2026-06-04 11:48:28 +0530

    fix: move Bookmark model to its own file and export it

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,3 +2,4 @@ from app.models.user import User
 from app.models.post import Post
 from app.models.comment import Comment
 from app.models.like import likes
+from app.models.bookmark import Bookmark
